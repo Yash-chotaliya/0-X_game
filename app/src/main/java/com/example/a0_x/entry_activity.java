15acf590c593bab33de34aacaff46a1b4a3bf5d4
yash chotaliya <yashchotaliya187@gmail.com>
2022-12-18 10:36:37 +0530

    " register , login page"

---
+++ app/src/main/java/com/example/a0_x/entry_activity.java
@@ -0,0 +1,40 @@
+package com.example.a0_x;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class entry_activity extends AppCompatActivity {
+    private Button login;
+    private Button register;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry);
+
+        register = findViewById(R.id.register);
+        login = findViewById(R.id.login);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(entry_activity.this,register_page.class));
+                finish();
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(entry_activity.this,login_page.class));
+                finish();
+            }
+        });
+
+    }
+}
